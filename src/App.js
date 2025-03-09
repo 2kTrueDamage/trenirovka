37e79004cacd6eb8c34cd736ac053388aa315361
2kTrueDamage <chief.shukshin@yandex.ru>
2025-03-10 00:21:54 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
+import Blocks from './components/Blocks';
 
 function App() {
+
+  const user = {
+    firstName: "Roman",
+    lastName: "Shukshin"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header />
+      <main>
+        <section className="greeting">
+          <h2>Hello, {user.firstName}</h2>
+        </section>
+        <Blocks />
+      </main>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className="name">Шапка сайта</h1>
+        </div>
+    )
+}
